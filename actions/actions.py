2b07dd3d7bfbeef1feffb8209501ffe73b697b5a
rubyyyy <113765262+rubyyylttt@users.noreply.github.com>
2023-04-02 03:43:43 +0800

    database update

--- actions/actions.py
+++ actions/actions.py
@@ -25,82 +25,79 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+from typing import Any, Text, Dict, List
 
-def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-    """A dictionary to map required slots to
-        - an extracted entity
-        - intent: value pairs
-        - a whole message
-        or a list of them, where a first match will be picked"""
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-    return {
-        "age": [
-            self.from_entity(entity="integer", role="age"),
-            self.from_entity(entity="integer"),
-        ],
-        "weight": [
-            self.from_entity(entity="integer", role="weight"),
-            self.from_entity(entity="integer"),
-        ],
-        "height": [
-            self.from_entity(entity="integer", role="height"),
-            self.from_entity(entity="integer"),
-        ],
-    }
-   
-def validate_age(
-    self,
-    value: Text,
-    dispatcher: CollectingDispatcher,
-    tracker: Tracker,
-    domain: Dict[Text, Any],
-) -> Dict[Text, Any]:
-    """Validate age value."""
-    
-    requested_slot = tracker.get_slot("requested_slot")
-    age_slot = tracker.get_slot("age")
+food_db = {
+    'Pineapple Bun':350,
+    'Chicken Breast':155,
+    'Brown Rice':100,
+    'Bread':90,
+    'Ramen':500
+}
 
-    if requested_slot == "age":
-        return {"age": value}
-    elif age_slot:
-        return {"age": age_slot}
-    else:
-        return {"age": None}
+workout_db = {
+    'leg':['Leg press 2*15 reps \n Leg extension 2*15 reps \n Seated leg curl 2*15 reps \n Standing calf raises 2*15 reps',\
+        'Barbell squats 3*10 reps \n Romanian deadlifts 3*10 reps \n Leg press - 2*15 reps \n Seated calf raises 2*15 reps',\
+            'Bulgarian split squats 3*10 reps \n Barbell hip thrusts 3*10 reps \n Leg press - 3*15 reps \n Seated calf raises 2*15 reps',\
+                'Barbell front squats - 4*8 reps \n Barbell lunges 4*8 reps \n Leg press - 3*15 reps \n Standing calf raises 3*15 reps',\
+                    'Barbell back squats 5*5 reps \n Deadlifts 5*5 reps \n Leg press 4*15 reps \n Donkey calf raises 3*20 reps'],
+    'arm':['Bicep curls with dumbbells 2*15 reps \n Tricep extensions with dumbbells - 2*15 reps \n Standing dumbbell curls 2*15 reps \n Overhead tricep extensions with dumbbells 2*15 reps',\
+        'Hammer curls with dumbbells 3*12 reps \n Close-grip bench press with barbell 3*12 reps \n Cable bicep curls 3*12 reps \n Tricep pushdowns with cable machine 3*12 reps',\
+            'Barbell curls 4*10 reps \n Skull crushers with EZ bar 4*10 reps \n Preacher curls with dumbbells 4*10 reps \n Cable tricep extensions with rope attachment 4*10 reps',\
+                '',''],
+    'back':['','','','',''],
+    'hip':['','','','',''],
+    'core':['','','','',''],
+    'cardio':['','','','','']
+}
 
-def validate_weight(
-    self,
-    value: Text,
-    dispatcher: CollectingDispatcher,
-    tracker: Tracker,
-    domain: Dict[Text, Any],
-) -> Dict[Text, Any]:
-    """Validate weight value."""
-    
-    requested_slot = tracker.get_slot("requested_slot")
-    weight_slot = tracker.get_slot("weight")
+class IntensityCalculation(Action):
 
-    if requested_slot == "weight":
-        return {"weight": value}
-    elif weight_slot:
-        return {"weight": weight_slot}
-    else:
-        return {"weight": None}
+    def name(self) -> Text:
+        return "action_calculate_intensity"
 
-def validate_height(
-    self,
-    value: Text,
-    dispatcher: CollectingDispatcher,
-    tracker: Tracker,
-    domain: Dict[Text, Any],
-) -> Dict[Text, Any]:
-    """Validate height value."""
-    
-    requested_slot = tracker.get_slot("requested_slot")
-    height_slot = tracker.get_slot("height")
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-    if requested_slot == "height":
-        return {"height": value}
-    elif height_slot:
-        return {"height": age_slot}
-    else:
-        return {"height": None}
+        height = tracker.get_slot("height")
+        height = int(height)
+        height = height/100
+
+        weight = tracker.get_slot("weight")
+        weight = int(weight)
+
+        exerciseFreq = tracker.get_slot("exercise_freq")
+        exercise_freq = int(exercise_freq)
+
+        BMI = weight/(height*height)
+        if not (height and weight and exerciseFreq):
+            dispatcher.utter_message(text="There is not enough info to calculate yet...")
+        else:
+            height = tracker.get_slot("height")
+            height = int(height)
+            height = height/100
+  
+            weight = tracker.get_slot("weight")
+            weight = int(weight)
+
+            BMI = weight/(height*height)
+            if (exercise_freq <= 1):
+                intensity = 2
+            elif exercise_freq <3:
+                intensity = 3
+            else:
+                intensity = 4
+            if BMI < 18.5 or BMI > 25:
+                intensity -= 1
+                dispatcher.utter_message(text="i would suggest a mild workout intensity level {intensity}, is there any body parts you want to train on?")
+            else:
+                intensity += 1
+                dispatcher.utter_message(text="I would suggest a workout plan of intensity {intensity}, tell me a part you want to train.")
+        return[]                
+
+
